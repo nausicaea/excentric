7814fdc85a95bc50f2a347efd442e2119b181ad2
Eleanor Young <developer@nausicaea.net>
2026-09-19 11:12:01 +0200

    fix(bell_block_mixin): add early exists for non-bell blocks, for piston movement, and for block state changes

--- common/src/main/java/net/nausicaea/excentric/mixin/BellBlockMixin.java
+++ common/src/main/java/net/nausicaea/excentric/mixin/BellBlockMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BellBlock;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BellBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.nausicaea.excentric.VillageManager;
@@ -14,12 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BellBlock.class)
 abstract class BellBlockMixin extends BlockBehaviourMixin {
 	@Override
-	protected void villageMod$onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2,
-	    boolean bl, CallbackInfo ci) {
+	protected void villageMod$onPlace(BlockState prevState, Level level, BlockPos blockPos, BlockState newState,
+	    boolean movedByPiston, CallbackInfo ci) {
 		if (!(level instanceof ServerLevel serverLevel)) {
 			return;
 		}
-		if (!(level.getBlockEntity(blockPos) instanceof BellBlockEntity bbe)) {
+		if (!prevState.is(Blocks.BELL) || movedByPiston || prevState.is(newState.getBlock())) {
+			return;
+		}
+		if (!(serverLevel.getBlockEntity(blockPos) instanceof BellBlockEntity bbe)) {
 			return;
 		}
 		VillageManager.get(serverLevel.getServer()).onBlockPlace(serverLevel, (VillageRef) bbe, blockPos);
